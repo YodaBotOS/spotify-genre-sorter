3193cdf679c35ec2d87dde8f4e746b0be4ef4f73
proguy914629 <proguy914629@openrobot.xyz>
2022-04-01 11:29:49 +0700

    Fix

---
+++ music-genre-classification/src/config.py
@@ -0,0 +1,21 @@
+# ⚠️ WARNING ⚠️
+# DO NOT CHANGE THIS FILE UNLESS YOU KNOW WHAT YOU ARE DOING.
+# CHANGING THIS FILE WILL CAUSE THE CURRENT CODE TO NOT WORK PROPERLY AS EXPECTED.
+# IF YOU ARE LOOKING FOR THE CONFIG FILE, IT IS AT THE ROOT FOLDER OF THE REPOSITORY.
+
+GENRES = ['blues',
+          'classical',
+          'country',
+          'disco',
+          'hiphop',
+          'jazz',
+          'metal',
+          'pop',
+          'reggae',
+          'rock']
+
+# DEFINE PATHS
+DATAPATH = '../data/'
+RAW_DATAPATH = '../utils/raw_data.pkl'
+SET_DATAPATH = '../utils/set.pkl'
+MODELPATH = '../model/net.pt'
